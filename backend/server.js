16543a87003412d16c59dd8a1d5b1faa54114fca
MohdAinul <mohdainul01@gmail.com>
2026-06-10 15:26:49 +0530

    new news api

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("Stock News API Running");
 });
 
+app.get("/news", (req, res) => {
+  res.send("stock Market news");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
