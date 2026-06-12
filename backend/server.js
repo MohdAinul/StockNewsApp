85842e9e05a8f75cba55052c6d76b5b1cd0e6394
MohdAinul <mohdainul01@gmail.com>
2026-06-12 17:52:14 +0530

    add multiple rss sources

--- backend/server.js
+++ backend/server.js
@@ -12,27 +12,75 @@ app.get("/", (req, res) => {
   res.send("Stock News API Running");
 });
 app.get("/news", async (req, res) => {
-  try {
-    const feed = await parser.parseURL(
-      "https://economictimes.indiatimes.com/markets/rssfeeds/1977021501.cms",
-    );
-    const news = feed.items.slice(0, 10).map((item, index) => ({
-      id: index + 1,
-      title: item.title,
-      source: "Economic Times",
-      published: item.pubDate,
-      link: item.link,
-    }));
+  const feeds = [
+    {
+      name: "Economic Times",
+      url: "https://economictimes.indiatimes.com/markets/rssfeeds/1977021501.cms",
+    },
+    {
+      name: "LiveMint",
+      url: "https://www.livemint.com/rss/markets",
+    },
+    {
+      name: "Moneycontrol",
+      url: "https://www.moneycontrol.com/rss/marketreports.xml",
+    },
+    {
+      name: "Economic TImes",
+      url: "https://economictimes.indiatimes.com/markets/stocks/rssfeeds/2146842.cms",
+    },
+    {
+      name: "Trade Brains",
+      url: "https://tradebrains.in/feed/",
+    },
+  ];
 
-    res.json(news);
+  try {
+    const allNews = [];
+
+    for (const feedSource of feeds) {
+      try {
+        const feed = await parser.parseURL(feedSource.url);
+
+        const news = feed.items.slice(0, 5).map((item, index) => ({
+          id: `${feedSource.name}-${index}`,
+          title: item.title || "No Title",
+          source: feedSource.name,
+          published: item.pubDate || item.isoDate || new Date().toISOString(),
+          link: item.link || "#",
+        }));
+
+        console.log(`Fetched ${news.length} news from ${feedSource.name}`);
+
+        allNews.push(...news);
+      } catch (error) {
+        console.log(`Failed to fetch ${feedSource.name}`);
+      }
+    }
+
+    // Remove duplicate titles
+    const uniqueNews = [];
+    const seenTitles = new Set();
+
+    for (const article of allNews) {
+      if (!seenTitles.has(article.title)) {
+        seenTitles.add(article.title);
+        uniqueNews.push(article);
+      }
+    }
+
+    // Latest news first
+    uniqueNews.sort((a, b) => new Date(b.published) - new Date(a.published));
+
+    res.json(uniqueNews);
   } catch (error) {
     console.error(error);
+
     res.status(500).json({
       error: "Failed to fetch news",
     });
   }
 });
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
