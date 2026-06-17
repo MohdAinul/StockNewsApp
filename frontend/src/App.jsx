d1367b9e8ac27bb12dd7c73b024bc44b8564cbb4
MohdAinul <mohdainul01@gmail.com>
2026-06-17 23:38:13 +0530

    set dark mode by default

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
   const API_URL = import.meta.env.VITE_API_URL;
 
